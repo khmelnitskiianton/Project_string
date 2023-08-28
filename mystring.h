12c45712226a6c576a58fae9a01f13ad398e6b78
ANTON <khmelnitskiianton@mail.ru>
2023-08-28 23:37:16 +0300

    my string functions like in lib
    This is my functions analogue to base functions from string.h

---
+++ mystring.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void my_puts(const char word[]);
+
+const char * my_strchr(const char word[], int symbol);
+
+size_t my_strlen(const char word[]);
+
+char * my_strcpy(char dest[], const char src[]);
+
+char * my_strncpy(char dest[], const char src[], size_t num);
+
+char * my_strcat( char dest[], const char src[]);
+
+char * my_strncat( char dest[], const char src[], size_t num);
+
+char * my_fgets( char word[], size_t num, FILE * filestream );
---
+++ mystring_test.cpp
@@ -0,0 +1,106 @@
+#include "mystring.h"
+
+
+void my_puts(const char word[])
+{
+    int i = 0;
+    char ch = '\n';
+    while ((ch = word[i]) != '\0')
+    {
+        putchar (ch);
+        i++;
+    }
+    putchar ('\n');
+}
+
+const char * my_strchr (const char word[], int symbol)
+{
+    int i = 0;
+    char ch = 0;
+    while ( (ch = word[i]) != symbol)
+        i++;
+    return &word[i];
+}
+
+size_t my_strlen(const char word[])
+{
+    int i = 0;
+    char ch = 0;
+    while ((ch = word[i]) != '\0')
+        i++;
+    return i;
+}
+
+char * my_strcpy(char dest[], const char src[])
+{
+    size_t i = 0;
+    while(src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }    
+    dest[i] = '\0';
+    return dest;
+}
+
+char * my_strncpy(char dest[], const char src[], size_t num)
+{
+    size_t i = 0;
+    while ((src[i] != '\0') && (i < num))
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    if (src[i] == '\0')
+        dest[i] = '\0';
+    return dest;
+}
+
+char * my_strcat(char dest[], const char src[])
+{
+    size_t i = 0;
+    while (dest[i] != '\0')
+        i++;
+    size_t j = 0;
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
+char * my_strncat( char dest[], const char src[], size_t num)
+{
+    size_t i = 0;
+    while (dest[i] != '\0')
+        i++;
+    size_t j = 0;
+    while ((src[j] != '\0')&&(j < num))
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
+char * my_fgets( char word[], size_t num, FILE * filestream )
+{
+    size_t i = 0;
+    int flag = 1; 
+    char ch = 0;
+    while ((i < num) && flag )
+    {
+        ch = getc(filestream);
+        if (ch == '\n')
+            flag = 0;
+        word[i] = ch;
+        i++;
+    }
+    word[i] = '\0';
+    return word;
+}
